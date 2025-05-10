0fcedd7ab6e90d2842cb0c3cb7789f1a614c0911
your_name <orkunkorcak@gmail.com>
2025-05-10 17:38:19 +0300

    edit nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,52 +2,63 @@ import { NavLink } from "react-router-dom";
 import css from "./Navigation.module.css";
 import Icon from "../../assets/Icons";
 import useMedia from "../../hooks/useMedia";
+import Balance from "../Balance/Balance";
+import Currency from "../Currency/Currency";
 
 const Navigation = () => {
   const { isMobile, isTablet, isDesktop } = useMedia();
 
   return (
-    <nav className={css.navigation}>
-      <NavLink
-        to="/dashboard/home"
-        className={({ isActive }) =>
-          isActive ? `${css.navLink} ${css.active}` : css.navLink
-        }
-      >
-        <div className={css.linkIcon}>
-          <Icon id="#icon-home" className={css.homeIcon} />
-        </div>
-        {(isTablet || isDesktop) && <span className={css.linkText}>Home</span>}
-      </NavLink>
+    <div className={css.container}>
+      
+        <nav className={css.navigation}>
+          <NavLink
+            to="/dashboard/home"
+            className={({ isActive }) =>
+              isActive ? `${css.navLink} ${css.active}` : css.navLink
+            }
+          >
+            <div className={css.linkIcon}>
+              <Icon id="#icon-home" className={css.homeIcon} />
+            </div>
+            {(isTablet || isDesktop) && (
+              <span className={css.linkText}>Home</span>
+            )}
+          </NavLink>
 
-      <NavLink
-        to="/dashboard/statistics"
-        className={({ isActive }) =>
-          isActive ? `${css.navLink} ${css.active}` : css.navLink
-        }
-      >
-        <div className={css.linkIcon}>
-          <Icon id="#icon-graphic" className={css.graphicIcon} />
-        </div>
-        {(isTablet || isDesktop) && (
-          <span className={css.linkText}>Statistics</span>
-        )}
-      </NavLink>
+          <NavLink
+            to="/dashboard/statistics"
+            className={({ isActive }) =>
+              isActive ? `${css.navLink} ${css.active}` : css.navLink
+            }
+          >
+            <div className={css.linkIcon}>
+              <Icon id="#icon-graphic" className={css.graphicIcon} />
+            </div>
+            {(isTablet || isDesktop) && (
+              <span className={css.linkText}>Statistics</span>
+            )}
+          </NavLink>
 
-      {isMobile && (
-        <NavLink
-          to="/dashboard/currency"
-          className={({ isActive }) =>
-            isActive ? `${css.navLink} ${css.active}` : css.navLink
-          }
-        >
-          <div className={css.linkIcon}>
-            <Icon id="#icon-dollar" className={css.dollarIcon} />
+          {isMobile && (
+            <NavLink
+              to="/dashboard/currency"
+              className={({ isActive }) =>
+                isActive ? `${css.navLink} ${css.active}` : css.navLink
+              }
+            >
+              <div className={css.linkIcon}>
+                <Icon id="#icon-dollar" className={css.dollarIcon} />
+              </div>
+            </NavLink>
+          )}
+        </nav>
+          <div>
+            <Balance />
           </div>
-        </NavLink>
-      )}
-
-    </nav>
+          {!isMobile && <Currency />}
+      
+    </div>
   );
 };
 
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Navigation from "../../components/Navigation/Navigation";
-import Balance from "../../components/Balance/Balance";
+// import Balance from "../../components/Balance/Balance";
 
 const DashboardPage = () => {
   return (
@@ -9,7 +9,6 @@ const DashboardPage = () => {
       
       <Header />
       <Navigation />
-      <Balance />
       <Outlet />
     </div>
   );
