afcebbc25cb126d099f82a4a497a31501f04eb46
batuhangozcu <batuhangozcu1@gmail.com>
2025-05-10 15:36:29 +0300

    Login & Register pages styling done

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,8 +1,15 @@
 import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
 import Icon from "../../assets/Icons";
 import styles from "./RegistrationPage.module.css";
+import { useEffect } from "react";
 
 const RegistrationPage = () => {
+  useEffect(() => {
+    document.body.classList.add(styles.registerPageBody);
+    return () => {
+      document.body.classList.remove(styles.registerPageBody);
+    };
+  }, []);
   return (
     <div className={styles.registrationPage}>
       <Icon id="#icon-logo-mobile" />
